64752c452514a3d91c0b12d5fa9b332c4440b5cc
xkenn <ervin.gonzales.inno@gmail.com>
2024-12-17 13:41:44 +0800

    add responsiveness to about page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { PiCopyrightThin } from "react-icons/pi";
 
 const Footer = () => {
   return (
-    <div className=" flex  items-center justify-center px-5  w-full bg-white/85 border-t-[1px] border-black ">
+    <div className=" flex  items-center justify-center px-5 py-2 w-full bg-white/85 border-t-[1px] border-black ">
       <p className="flex flex-row items-center justify-center gap-x-1 font-serif font-light">
         <PiCopyrightThin size={20} /> 2024 Gonzales Ervin
       </p>
